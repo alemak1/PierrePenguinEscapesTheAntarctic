c5a4e451c2bc086ded0a559005929bbc903f5fa7
yongrenwang <czie.alexmak@gmail.com>
2017-01-27 18:37:39 +0800

    Loaded image assets into assets folder; added first bee to scene throuh didMoveToView function; deleted boilderplate code in default generated GameViewController

--- PierrePenguinEscapesFromTheAntarctic/PierrePenguinEscapesFromTheAntarctic/GameViewController.swift
+++ PierrePenguinEscapesFromTheAntarctic/PierrePenguinEscapesFromTheAntarctic/GameViewController.swift
@@ -12,24 +12,19 @@ import GameplayKit
 
 class GameViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        let scene = GameScene()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+        let skView = self.view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        
+        scene.scaleMode = .aspectFill
+        scene.size = view.bounds.size
+        skView.presentScene(scene)
     }
 
     override var shouldAutorotate: Bool {
