34fdb0be252178f2cbff74715174ec4dc83baea8
yongrenwang <czie.alexmak@gmail.com>
2017-01-27 21:06:53 +0800

    Created a Ground class

---
+++ PierrePenguinEscapesFromTheAntarctic/PierrePenguinEscapesFromTheAntarctic/Ground.swift
@@ -0,0 +1,56 @@
+//
+//  Ground.swift
+//  PierrePenguinEscapesFromTheAntarctic
+//
+//  Created by Aleksander Makedonski on 1/27/17.
+//  Copyright © 2017 Changzhou Panda. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class Ground: SKSpriteNode, GameSprite{
+    var textureAtlas: SKTextureAtlas = SKTextureAtlas(named: "ground.atlas")
+    var groundTexture: SKTexture?
+    
+    func spawn(parentNode: SKNode, position: CGPoint, size: CGSize) {
+        parentNode.addChild(self)
+        self.size = size
+        self.position = position
+        
+        self.anchorPoint = CGPoint(x: 0, y: 1)
+        
+        //default to the ice texture
+        if groundTexture == nil {
+            groundTexture = textureAtlas.textureNamed("ice-tile.png")
+        }
+        
+        createChildren()
+        
+    }
+    
+    func createChildren(){
+        
+        if let texture = groundTexture{
+            var tileCount: CGFloat = 0
+            let textureSize = texture.size()
+            
+            let tileSize = CGSize(width: textureSize.width/2, height: textureSize.height/2)
+            
+            while tileCount*tileSize.width < self.size.width{
+                let tileNode = SKSpriteNode(texture: texture)
+                tileNode.size = tileSize
+                tileNode.position.x = tileCount*tileSize.width
+                tileNode.anchorPoint = CGPoint(x: 0, y: 1)
+                self.addChild(tileNode)
+                tileCount += 1
+                
+            }
+        }
+        
+        func onTap(){
+            
+        }
+        
+    }
+}
