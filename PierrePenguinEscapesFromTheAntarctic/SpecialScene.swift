d18caa9c721d17772b5454796ca82699be121532
yongrenwang <czie.alexmak@gmail.com>
2017-01-28 21:20:18 +0800

    Added a new folder 'Encounters' to manage encounter scenes; added a new scene for bat encounter

---
+++ PierrePenguinEscapesFromTheAntarctic/SpecialScene.swift
@@ -0,0 +1,19 @@
+//
+//  SpecialScene.swift
+//  PierrePenguinEscapesFromTheAntarctic
+//
+//  Created by Aleksander Makedonski on 1/28/17.
+//  Copyright © 2017 Changzhou Panda. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+import GameplayKit
+
+class SpecialScene: SKScene{
+    
+    override func didMove(to view: SKView) {
+        super.didMove(to: view)
+    }
+    
+}
