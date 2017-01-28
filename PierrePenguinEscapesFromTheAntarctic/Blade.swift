581041bcb5c82e410cb9d379a124c2a0ff35e352
yongrenwang <czie.alexmak@gmail.com>
2017-01-28 13:53:04 +0800

    Added the Blade class to the project directory

---
+++ PierrePenguinEscapesFromTheAntarctic/Blade.swift
@@ -0,0 +1,41 @@
+//
+//  Blade.swift
+//  PierrePenguinEscapesFromTheAntarctic
+//
+//  Created by Aleksander Makedonski on 1/28/17.
+//  Copyright © 2017 Changzhou Panda. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class Blade: SKSpriteNode, GameSprite{
+    
+    var textureAtlas: SKTextureAtlas = SKTextureAtlas(named: "enemies.atlas")
+    var spinAnimation = SKAction()
+    
+    func spawn(parentNode: SKNode, position: CGPoint, size: CGSize = CGSize(width: 185, height: 92)) {
+        parentNode.addChild(self)
+        self.size = size
+        self.position = position
+        self.physicsBody = SKPhysicsBody(texture: textureAtlas.textureNamed("blade-1.png"), size: size)
+        self.physicsBody?.affectedByGravity = false
+        self.physicsBody?.isDynamic = false
+        createAnimations()
+        self.run(spinAnimation)
+    }
+    
+    func onTap() {
+        
+    }
+    
+    func createAnimations(){
+        let spinFrames: [SKTexture] = [
+            textureAtlas.textureNamed("blade-1.png"),
+            textureAtlas.textureNamed("blade-2.png")
+        ]
+        
+        let spinAction = SKAction.animate(with: spinFrames, timePerFrame: 0.07)
+        spinAnimation = SKAction.repeatForever(spinAction)
+    }
+}
