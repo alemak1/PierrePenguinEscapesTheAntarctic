f3831b92a4d9a3f7f58cdb93c83c632745ed3a1d
yongrenwang <czie.alexmak@gmail.com>
2017-01-29 19:12:04 +0800

    Added arrays for character images and character names

--- CharacterIntros/CharacterIntros/ViewController.swift
+++ CharacterIntros/CharacterIntros/ViewController.swift
@@ -10,6 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let images = [
+        #imageLiteral(resourceName: "Icon-76"),
+        #imageLiteral(resourceName: "ghost-frown"),
+        #imageLiteral(resourceName: "bee"),
+        #imageLiteral(resourceName: "mad-fly-1"),
+        #imageLiteral(resourceName: "blade-1"),
+        #imageLiteral(resourceName: "coin-gold"),
+        #imageLiteral(resourceName: "coin-bronze")
+    ]
+    
+    let imageNames: [String] = [
+        "Icon-76",
+        "ghost-frown",
+        "bee",
+        "mad-fly-1",
+        "blade-1",
+        "coin-gold",
+        "coin-bronze"
+    ]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
