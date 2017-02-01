a218768b7fd2ad61ff73abef0fc5e0092e409a02
yongrenwang <czie.alexmak@gmail.com>
2017-02-01 17:53:27 +0800

    Completed defining the damageAnimation and finished implementing the takeDamage function

--- PierrePenguinEscapesFromTheAntarctic/PierrePenguinEscapesFromTheAntarctic/Player.swift
+++ PierrePenguinEscapesFromTheAntarctic/PierrePenguinEscapesFromTheAntarctic/Player.swift
@@ -87,6 +87,52 @@ class Player: SKSpriteNode, GameSprite{
             SKAction.repeatForever(soarAction),
             rotateDownAction
             ])
+        
+        //Create the taking damage animation
+        let damageStart = SKAction.run({
+            //Allow the penguin to pass through enemies
+            self.physicsBody?.categoryBitMask = PhysicsCategory.damagedPenguin.rawValue
+            
+            //Use the bitwise NOT operator ~ to remove enemies from the collision test
+            self.physicsBody?.collisionBitMask = ~PhysicsCategory.enemy.rawValue
+            
+        })
+        
+        //Create an opacity pulse, slow at first and fast at the end
+        let slowFade = SKAction.sequence([
+            SKAction.fadeAlpha(to: 0.3, duration: 0.35),
+            SKAction.fadeAlpha(to: 0.7, duration: 0.35)
+            ])
+        
+        let fastFade = SKAction.sequence([
+            SKAction.fadeAlpha(to: 0.3, duration: 0.20),
+            SKAction.fadeAlpha(to: 0.7, duration: 0.20)
+            ])
+        
+        let fadeOutAndIn = SKAction.sequence([
+            SKAction.repeat(slowFade, count: 2),
+            SKAction.repeat(fastFade, count: 5),
+            SKAction.fadeAlpha(to: 1, duration: 0.15)
+            ])
+        
+        let damageEnd = SKAction.run({
+            //Return the penguin to normal
+            self.physicsBody?.categoryBitMask = PhysicsCategory.penguin.rawValue
+            
+            //Collide with everything again
+            self.physicsBody?.collisionBitMask = 0xFFFFFFFF
+            
+            //Turn off the newly damaged flag
+            self.damaged = false
+            
+        })
+        
+        //Store the whole sequence in the damage animation
+        self.damageAnimation = SKAction.sequence([
+            damageStart,
+            fadeOutAndIn,
+            damageEnd
+            ])
     }
     
     func startFlapping(){
@@ -130,6 +176,9 @@ class Player: SKSpriteNode, GameSprite{
     func takeDamage(){
         //If invulnerable or damaged, return:
         if self.invulnerable || self.damaged { return }
+        
+        //set the damaged state to true after being hit:
+        self.damaged = true
         
         //Remove one from our health pool
         self.health -= 1
